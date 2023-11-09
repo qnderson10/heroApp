54c6f6fd2f99bbd0117140a91bc86cab60cf97cf
Anderson Gonzalez <andersongonzalez2000@gmail.com>
2023-11-09 18:11:38 -0500

    Endpoints del CRUD de los heroes implementado

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'heroes-new-page',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-page.component.css']
 })
 export class NewPageComponent {
+  public heroForm = new FormGroup({
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('', {nonNullable: true}),
+    publisher:        new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego:        new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters:       new FormControl<string>(''),
+    alt_img:          new FormControl<string>(''),
+  });
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics', },
     { id: 'Marvel Comics', desc: 'Marvel - Comics', }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -24,5 +24,24 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]>{
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero): Observable<Hero>{
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero>{
+    if (!hero.id) {
+      throw Error('Hero id is required')
+    }
+    return this.httpClient.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHero(id: string): Observable<boolean>{
+    return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(err => of(false)),
+        map(resp => true)
+      )
   }
 }
