a68a9b967950574a12df03e0813674393d269692
Anderson Gonzalez <andersongonzalez2000@gmail.com>
2023-11-14 21:01:00 -0500

    Funciones añadir y eliminar heroes implementados

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, Pipe } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { HeroesService } from '../../services/heroes.service';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { filter, switchMap, tap } from 'rxjs';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'heroes-new-page',
   templateUrl: './new-page.component.html',
   styleUrls: ['./new-page.component.css']
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
   public heroForm = new FormGroup({
     id:               new FormControl<string>(''),
     superhero:        new FormControl<string>('', {nonNullable: true}),
@@ -22,4 +30,90 @@ export class NewPageComponent {
     { id: 'DC Comics', desc: 'DC - Comics', },
     { id: 'Marvel Comics', desc: 'Marvel - Comics', }
   ];
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog,
+  ){}
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) {
+      return;
+    }
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesService.getHeroById(id)),
+      ).subscribe(hero => {
+        if (!hero) {
+          return this.router.navigateByUrl('/');
+        }
+
+        this.heroForm.reset(hero);
+        return;
+      });
+  }
+
+  get currentHero():Hero{
+    const hero = this.heroForm.value as Hero;
+
+    return hero;
+  }
+  onSubmit():void{
+    if(this.heroForm.invalid){ return; }
+
+    if(this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe(hero => {
+          this.showSnackbar(`${hero.superhero} updated`);
+        });
+      return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+      .subscribe(hero => {
+        this.router.navigate(['/heroes/edit', hero.id]);
+        this.showSnackbar(`${hero.superhero} created`);
+      });
+  }
+
+  onDeleteHero():void{
+    if (!this.currentHero.id) {
+      throw Error('Hero id is required');
+    }
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value,
+    });
+
+    dialogRef.afterClosed()
+      .pipe(
+        filter((result: boolean) => result),
+        switchMap(() => this.heroesService.deleteHero(this.currentHero.id)),
+        filter((wasDeleted: boolean) => wasDeleted),
+      )
+      .subscribe(result => {
+        this.router.navigateByUrl('/');
+      });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if(!result){ return;}
+
+    //   this.heroesService.deleteHero(this.currentHero.id)
+    //     .subscribe(wasDeleted => {
+    //       if (wasDeleted) {
+    //         this.router.navigateByUrl('/');
+    //       }
+    //     });
+      // this.animal = result;
+    // });
+  }
+
+  showSnackbar(meesage: string):void{
+    this.snackbar.open(meesage, 'done', {
+      duration: 3000,
+    });
+  }
 }
