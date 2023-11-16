15d30c9f2938ef7b8aaae02f4140a31072bf9954
Anderson Gonzalez <andersongonzalez2000@gmail.com>
2023-11-16 11:39:41 -0500

    Guards basicos implementados para los usuarios

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environments } from 'src/environments/environments';
+import { User } from '../interfaces/user.interface';
+import { Observable, catchError, map, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environments.baseUrl;
+  private user?: User;
+
+  constructor(private httpClient: HttpClient) { }
+
+  get currentUser(): User | undefined{
+    if (!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string): Observable<User>{
+    return this.httpClient.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        tap(user => localStorage.setItem('token', user.id.toString())),
+      );
+  }
+
+  logout():void {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+  checkAuthentication(): Observable<boolean>{
+    if(!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+    return this.httpClient.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError(err => of(false)),
+      );
+  }
+}
